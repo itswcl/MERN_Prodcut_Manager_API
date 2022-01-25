38b03a39cc63926acea2d55ceb0a4d8aabfc4b79
weicheng lee <z82111712@gmail.com>
2022-01-24 18:02:25 -0800

    add find one controller and route

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -3,22 +3,33 @@ const Product = require("../models/product.model");
 module.exports = {
     // index route to test if working
     index: (req, res) => {
-        res.json({message: "Hello World"})
+        res.json({ message: "Hello World" })
     },
-
-    // Read All
-    findProducts: (req, res) => {
-        Product.find()
-            .then(products => res.json({products: products, message: "success"}))
-    },
-
-    createProduct: (req, res) => {
-        const {title, price, description} = req.body;
+// ------------------------------ CRUD ------------------------------
+    // ---------------- Create ----------------
+    createOne: (req, res) => {
+        const { title, price, description } = req.body;
         Product.create({
             title,
             price,
             description
-        }).then(person => res.json({person: person.data}))
+        }).then(person => res.json({ person: person.data }))
             .catch(err => res.json(err))
+    },
+
+
+    // ---------------- Read All ----------------
+    findAll: (req, res) => {
+        Product.find()
+            .then(products => res.json({ products: products, message: "success" }))
+    },
+
+
+    // ---------------- Read One ----------------
+    findOne: (req, res) => {
+        // Product.findOne({_id: req.params.id})
+        Product.findById(req.params.id)
+            .then(product => res.json({product: product, message: "success"}))
+            .catch(err => res.json({message: "failed", error: err}))
     }
 }
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,6 +2,9 @@
 const ProductController = require('../controllers/product.controller')
 module.exports = function(app) {
     app.get("/", ProductController.index);
-    app.get("/api/products", ProductController.findProducts)
-    app.post("/api/products/new", ProductController.createProduct)
+    app.get("/api/products", ProductController.findAll)
+    // :id should match the findById(req.params.id) in controller
+    app.get("/api/products/:id", ProductController.findOne)
+    app.post("/api/products/new", ProductController.createOne)
+
 }
