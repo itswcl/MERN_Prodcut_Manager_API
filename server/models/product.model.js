83ec38e0ae6c5ed2e029084e908906cc37efdc45
weicheng lee <z82111712@gmail.com>
2022-01-24 17:37:55 -0800

    update api route for all products

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const ProductSchema = new mongoose.Schema({
-    title: { type: String },
-    price: { type: String },
-    description: { type: String }
+    title: { type: String, require: [true, "{PATH} required"] },
+    price: { type: Number, require: [true, "price required"] },
+    description: { type: String, require: [true, "{PATH} required"] }
 }, { timestamps: true });
 
 const Product = mongoose.model('Product', ProductSchema);
